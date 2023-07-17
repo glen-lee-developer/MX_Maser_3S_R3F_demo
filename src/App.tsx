eca3439db25924ccb12c0fbc0faba6c661f0c579
glen-lee-developer <glenleecareer@gmail.com>
2023-07-17 03:28:59 +0100

    add background to canvas instead of css but need to fix encoding

--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,41 @@ import { Suspense, useState } from "react";
 import { ConfigColoursType } from "./types/configColoursType";
 import ConfigurationButtons from "./components/configurationButtons";
 
-
 function App() {
-	const [configuration, setConfiguration] = useState<ConfigColoursType>("black")
+  const [configuration, setConfiguration] =
+    useState<ConfigColoursType>("black");
 
-	return (
-		<>
-			<Canvas
-			dpr={[1, 2]}
-			camera={{
-				fov: 64,
-				position: [0, 0, 0],
-			}}
-			style={{
-				// position: "fixed",
-				// width: "100%",
-				// height: "100%",
-				// zIndex: -1,
-			}}
-			>
-			<ambientLight intensity={0.2} />
-			<directionalLight position={[-3.3, -1.0, 4.4]} intensity={2} />
-			<directionalLight position={[3.3, 1.0, -4.4]} intensity={2} />
-			{/* <OrbitControls enableZoom /> */}
-			<Suspense fallback={null}>
-				<Experience configuration={configuration} />
-			</Suspense>
-		</Canvas>
-		
-		<ConfigurationButtons configuration={configuration} setConfiguration={setConfiguration}/>
-		</>
-	);
+  return (
+    <>
+      <Canvas
+        dpr={[1, 2]}
+        camera={{
+          fov: 64,
+          position: [0, 0, 0],
+        }}
+        style={{
+          background: "no-repeat center center fixed",
+          // position: "fixed",
+          // width: "100%",
+          // height: "100%",
+          // zIndex: -1,
+        }}
+      >
+        <ambientLight intensity={0.2} />
+        <directionalLight position={[-3.3, -1.0, 4.4]} intensity={2} />
+        <directionalLight position={[3.3, 1.0, -4.4]} intensity={2} />
+        {/* <OrbitControls enableZoom /> */}
+        <Suspense fallback={null}>
+          <Experience configuration={configuration} />
+        </Suspense>
+      </Canvas>
+
+      <ConfigurationButtons
+        configuration={configuration}
+        setConfiguration={setConfiguration}
+      />
+    </>
+  );
 }
 
 export default App;
--- src/components/experience/index.tsx
+++ src/components/experience/index.tsx
@@ -1,23 +1,27 @@
 import { ScrollControls } from "@react-three/drei";
+import { ConfigColoursType } from "../../types/configColoursType";
+import * as THREE from "three";
+import { useThree } from "@react-three/fiber";
 import Overlay from "../overlay";
 import Mouse from "../mouse";
-import { ConfigColoursType } from "../../types/configColoursType";
 
 type ExperienceProps = {
-	configuration:ConfigColoursType;
-}
-
-export const Experience = ({ configuration }:ExperienceProps) => {
-		return (
-			<>
-			<ScrollControls pages={9} >
-					<Overlay  />
-					<Mouse configuration={configuration} />
-				</ScrollControls>
-			</>
-	)
+  configuration: ConfigColoursType;
 };
 
+export const Experience = ({ configuration }: ExperienceProps) => {
+  const { scene } = useThree();
+  const loader = new THREE.TextureLoader();
+  const texture = loader.load("./backgrounds/darkBackground.jpg");
+  scene.background = texture;
+  scene.background.encoding = THREE.sRGBEncoding;
 
-
-
+  return (
+    <>
+      <ScrollControls pages={9}>
+        <Overlay />
+        <Mouse configuration={configuration} />
+      </ScrollControls>
+    </>
+  );
+};
