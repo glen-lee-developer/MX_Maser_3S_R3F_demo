d061fef3bf15090ef96d78d36d9609052cfaa2d1
glen-lee-developer <glenleecareer@gmail.com>
2023-07-17 02:08:32 +0100

    temporarily change remove GLTF Type as causing bug and needs time to fix

--- src/types/MouseGLTFType.ts
+++ src/types/MouseGLTFType.ts
@@ -1,33 +1,33 @@
 import { GLTF } from "three/examples/jsm/loaders/GLTFLoader";
 
 export type MouseGLTFType = GLTF & {
-	nodes: {
-		Mx_Master_3_Mouse: THREE.Group;
-		mesh_0: THREE.Mesh;
-		mesh_0_1: THREE.Mesh;
-		mesh_0_2: THREE.Mesh;
-		mesh_0_3: THREE.Mesh;
-		mesh_0_4: THREE.Mesh;
-		mesh_0_5: THREE.Mesh;
-		mesh_0_6: THREE.Mesh;
-		mesh_0_7: THREE.Mesh;
-		mesh_0_8: THREE.Mesh;
-		mesh_0_9: THREE.Mesh;
-		mesh_0_10: THREE.Mesh;
-		mesh_0_11: THREE.Mesh;
-	};
-	materials: {
-		Aluminium: THREE.MeshStandardMaterial;
-		Base: THREE.MeshStandardMaterial;
-		Base2: THREE.MeshStandardMaterial;
-		Black: THREE.MeshStandardMaterial;
-		Chrome: THREE.MeshStandardMaterial;
-		Glass: THREE.MeshStandardMaterial;
-		Green_button: THREE.MeshStandardMaterial;
-		Led_closed: THREE.MeshStandardMaterial;
-		Logo: THREE.MeshStandardMaterial;
-		Plastic: THREE.MeshStandardMaterial;
-		Plastic_panel: THREE.MeshStandardMaterial;
-		USB_ss: THREE.MeshStandardMaterial;
-	};
+  nodes: {
+    Mx_Master_3_Mouse: THREE.Group;
+    mesh_0: THREE.Mesh;
+    mesh_0_1: THREE.Mesh;
+    mesh_0_2: THREE.Mesh;
+    mesh_0_3: THREE.Mesh;
+    mesh_0_4: THREE.Mesh;
+    mesh_0_5: THREE.Mesh;
+    mesh_0_6: THREE.Mesh;
+    mesh_0_7: THREE.Mesh;
+    mesh_0_8: THREE.Mesh;
+    mesh_0_9: THREE.Mesh;
+    mesh_0_10: THREE.Mesh;
+    mesh_0_11: THREE.Mesh;
+  };
+  materials: {
+    Aluminium: THREE.MeshStandardMaterial;
+    Base: THREE.MeshStandardMaterial;
+    Base2: THREE.MeshStandardMaterial;
+    Black: THREE.MeshStandardMaterial;
+    Chrome: THREE.MeshStandardMaterial;
+    Glass: THREE.MeshStandardMaterial;
+    Green_button: THREE.MeshStandardMaterial;
+    Led_closed: THREE.MeshStandardMaterial;
+    Logo: THREE.MeshStandardMaterial;
+    Plastic: THREE.MeshStandardMaterial;
+    Plastic_panel: THREE.MeshStandardMaterial;
+    USB_ss: THREE.MeshStandardMaterial;
+  };
 };
